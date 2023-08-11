d87c89d951048d22a52b3e79f9aa40d9d9738425
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-08-11 14:25:38 +0100

    first task done

--- farm.js
+++ farm.js
@@ -1,12 +1,15 @@
 // Level 1
-function cropWateringCalculator(forecast){
-  
+function cropWateringCalculator(forecast) {
+  let count = 0;
+  for (let i = 0; i < forecast.length; i++) {
+    if (forecast[i].weather !== "rain") {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 // Level 2 and 3
-function cropWateringCalculatorImproved(forecast, numOfCrops){
+function cropWateringCalculatorImproved(forecast, numOfCrops) {}
 
-}
-
-module.exports = { cropWateringCalculator, cropWateringCalculatorImproved }
-
+module.exports = { cropWateringCalculator, cropWateringCalculatorImproved };
--- farm.test.js
+++ farm.test.js
@@ -1,1 +1,41 @@
 // your code here
+const { cropWateringCalculator } = require("./farm");
+
+describe("cropWateringCalculator", () => {
+  test("should return 0 for an empty forecast", () => {
+    const forecast = [];
+
+    const result = cropWateringCalculator(forecast);
+
+    expect(result).toBe(0);
+  });
+
+  test("should return 0 when there are all rainy days", () => {
+    const forecast = [{ day: 1, weather: "rain" }];
+
+    const result = cropWateringCalculator(forecast);
+
+    expect(result).toBe(0);
+  });
+
+  test('should return 1 when there is only one non-"rain" forecast', () => {
+    const forecast = [{ day: 1, weather: "sunny" }];
+
+    const result = cropWateringCalculator(forecast);
+
+    expect(result).toBe(1);
+  });
+
+  test('should return the count of non-"rain" weather forecasts', () => {
+    const forecast = [
+      { day: 1, weather: "rain" },
+      { day: 2, weather: "sunny" },
+      { day: 3, weather: "cloudy" },
+      { day: 4, weather: "sunny" },
+    ];
+
+    const result = cropWateringCalculator(forecast);
+
+    expect(result).toBe(3);
+  });
+});
